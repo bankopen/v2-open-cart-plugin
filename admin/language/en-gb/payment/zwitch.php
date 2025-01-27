27c066bcc37dd85af16163fe1365258ebc0eab76
jestin.s <jestin.sebsastian@bankopen.co>
2025-01-27 13:05:49 +0530

    New Plugins

---
+++ admin/language/en-gb/payment/zwitch.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']         = 'ZWITCH';
+
+$_['entry_secret_key']      = 'Secret Key';
+$_['entry_access_key']      = 'Access Key';
+$_['entry_geo_zone']		= 'Geo Zone';
+$_['entry_order_status']    = 'Confirmed Order Status';
+$_['entry_sort_order']      = 'Sort Order';
+$_['entry_status']          = 'Status';
+$_['entry_page_color']      = 'Payment Page Color';
+$_['entry_test_mode']       = 'Environment Mode';
+
+// Text
+$_['text_extension']        = 'Extensions';
+$_['text_success']          = 'Success: You have modified ZWITCH credentials!';
+$_['text_edit']             = 'Edit ZWITCH';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify ZWITCH credentials!';
+$_['error_secret_key']      = 'Warning: Secret Key is required!';
+$_['error_access_key']      = 'Warning: Access Key is required!';
